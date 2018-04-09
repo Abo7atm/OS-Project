913c36c9fce0e72720e8274d0fe13fa2046faa4a
abo7atm <aaalnumay@gmail.com>
2018-04-09 22:24:32 +0300

    protected

--- JobScheduler.java
+++ JobScheduler.java
@@ -1,4 +1,24 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class JobScheduler {
-	LinkedPQ<PCB> list = new LinkedPQ<PCB>();
+	LinkedPQ<PCB> list;
+	String file;
+	
+	public JobScheduler(String file) {
+		list = new LinkedPQ<PCB>();
+		this.file = file;
+	}
+	
+	public void readFile() throws FileNotFoundException {
+		File f = new File(this.file);
+		FileReader fr = new FileReader(f);
+		BufferedReader br = new BufferedReader(fr);
+		
+		
+	}
+	
 }

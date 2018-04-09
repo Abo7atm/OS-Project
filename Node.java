2fdc0e3f3380c5bb08d67ab17c835adfd459548d
abo7atm <aaalnumay@gmail.com>
2018-04-09 21:55:29 +0300

    added output.txt

--- Node.java
+++ Node.java
@@ -1,4 +1,16 @@
 
 public class Node {
-
+	private Process data;
+	private Node next;
+	public Node(Process p) {
+		this.data = p;
+	}
+	
+	public void setNext(Node n) {
+		next = n;
+	}
+	
+	public Node getNext() {
+		return next;
+	}
 }

c75fc052e18158e1fbe94761068994e7c9ce20f8
abo7atm <aaalnumay@gmail.com>
2018-04-09 22:49:51 +0300

    finished file reader

--- JobScheduler.java
+++ JobScheduler.java
@@ -13,14 +13,25 @@ public class JobScheduler {
 		this.file = file;
 	}
 	
-	public void readFile() throws FileNotFoundException {
+	public void readFile() throws IOException {
 		File f = new File(this.file);
 		FileReader fr = new FileReader(f);
 		BufferedReader br = new BufferedReader(fr);
+		String line;
 		
-		String line = br.readLine();
-		
-		while(br.)
+		while((line=br.readLine()) != null) {
+			String[] splitted = line.split(";");
+			String idString = splitted[0].split(":")[1];
+			String cpuTimeString = splitted[1].split(":")[1];
+			String sizeString = splitted[2].split(":")[1];
+			
+			int id = Integer.parseInt(idString);
+			int cpuTime = Integer.parseInt(cpuTimeString);
+			int size = Integer.parseInt(sizeString);
+			
+			PCB result = new PCB(id, cpuTime, size);
+			list.enqueue(result, result.getSize());
+		}
 	}
 	
 	public PCB serve() {
